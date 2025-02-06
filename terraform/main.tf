36dbdcf37007fbbebe2f52bb3834541cb4dcae6f
tharukanandasiri <tharukanandasiri123@gmail.com>
2025-02-06 19:07:31 +0530

    terraform files created

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami             = "ami-12345678"  # Replace with the latest AMI ID
+  instance_type   = var.instance_type
+  subnet_id       = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.web_sg.name]
+  key_name        = var.key_name
+
+  tags = {
+    Name = "WebServer"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key pair for SSH access"
+  type        = string
+  default     = "my-key"
+}
